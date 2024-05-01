487dd6007c4d8350d3e525a29e77856157a71998
Shukri Hussein <husseinshukri@outlook.com>
2024-05-01 14:05:15 -0400

    front-end routes1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,50 +2,29 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import axios from "axios";
+
+import { Route, Routes } from "react-router-dom"
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import Groups from './pages/Groups';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [array, setArray] = useState([]);
 
-  const fetchAPI = async () => {
-    const response = await axios.get("http://127.0.0.1:8080/api/users")
-    console.log(response.data.users);
-    setArray(response.data.users);
-  };
-
-  useEffect(() => {
-    fetchAPI()
-
-  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/groups" element={<Groups />} />
+      </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-          {array.map((user, index) => (
-            <div key={index}>
-                <span>{user}</span>
-                <br></br>
-            </div>
-          ))}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </>
   )
 }
 
---
+++ client/src/pages/Groups.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Groups() {
+
+
+  return (
+    <div>This is groups page</div>
+  )
+}
+
+export default Groups
