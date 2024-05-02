a507cec5599a915432d42894fb09e73adcb80c04
Jasen <jasen.miyamoto@gmail.com>
2024-05-02 16:30:42 -0400

    routed all backend routes

--- server/main.py
+++ server/main.py
@@ -111,7 +111,60 @@ class UsersById(Resource):
 
 api.add_resource(UsersById, 'users/<int:id>')
 
+#jasen might be wrong about how this should work and is willing to accept responsibility 
+class GameStatsByGameID(Resource):
+    #this gets ALL comments, reviews for a specific game NOT BY USER
+    def get(self,game_id): 
+        gamestats = [gamestat.to_dict() for gamestat in GameStatistics.query.filter(GameStatistics.game_id==game_id).all()]
+        if gamestats:
+            return make_response(gamestats)
+        else:
+            return make_response({'error': ['No reviews yet']})
 
+api.add_resource(GameStatsByGameID, '/game-statistic/<int:game_id>')
+
+class GameStatsByUserAndGameIDs(Resource):
+    #this allows a user to patch, passing both ids in (i think)
+    def patch(self, game_id, user_id):
+        gamestats = GameStatistics.query.filter(GameStatistics.game_id == game_id and GameStatistics.user_id==user_id).first()
+        if not gamestats:
+            return make_response({"error": "User not found"}, 404)
+        else:
+            try:
+                for attr in request.json:
+                    setattr(gamestats, attr, request.json.get(attr))
+                
+                db.session.add(gamestats)
+                db.session.commit()
+
+                return make_response(gamestats.to_dict(), 202)
+            except:
+                return make_response({"errors": ["validation errors"]}, 400)
+
+api.add_resource(GameStatsByUserAndGameIDs, '/game-statstics/<int:game_id>/<int:user_id>')
+
+class GameStats(Resource):
+    #is this right???
+    def post(self):
+        data = request.json
+        try:
+            new_gamestats = GameStatistics(
+                user_id = data.get('user_id'),
+                game_id = data.get('game_id'),
+                comments = data.get('comments'),
+                rating = data.get('rating'),
+                favorited = data.get('favorited'),
+                wishlisted = data.get('wishlisted')
+            )
+            if new_gamestats:
+                db.session.add(new_gamestats)
+                db.session.commit()
+
+                return make_response(new_gamestats, 201)
+            else:
+                return make_response({'error': 'Review could not be made'}, 400) #TODO check code 
+        except:
+            return make_response({'error': ['validation errors']}, 400)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
