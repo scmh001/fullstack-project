51f4e45b422a6319cb1a264fc9d5b74b2d930976
kgrace61 <110522366+kgrace61@users.noreply.github.com>
2024-05-02 17:55:36 -0500

    games/id page / link to and from game page

--- client/src/pages/GameDetail.jsx
+++ client/src/pages/GameDetail.jsx
@@ -1,17 +1,59 @@
-// GameDetail.jsx
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import gameData from '@/assets/TEST.json';
+
+// THIS ENTIRE PAGE COULD ALSO BE A POP UP-PAGE ON GAMES ??
 
 function GameDetail() {
-  const { id } = useParams(); // Retrieve the game ID from the URL parameter
+  const { id } = useParams();
+  const [game, setGame] = useState(null);
 
-  // Fetch game details based on the ID and display them
+  useEffect(() => {
+    // find the game with the corresponding ID
+    const selectedGame = gameData.find(game => game.id === parseInt(id));
+    setGame(selectedGame);
+    // scrolls to the top when the component mounts
+    window.scrollTo(0, 0);
+  }, [id]);
+
+  if (!game) {
+    return <div>Loading...</div>; // displays loading indicator while fetching data
+  }
 
   return (
-    <div>
-      <h1>Game Detail</h1>
-      <p>Game ID: {id}</p>
-      {/* Display other details of the game */}
+    <div className="game-detail-container">
+      {/* title w/ minor css*/}
+      <h1 style={{ fontWeight: 'bold' }}>{game.name}</h1>
+      {/* image to match location of figma// can be changed however */}
+      <img src={game.image} alt={game.name} style={{ width: '50%', maxWidth: '300px' }} />
+      {/* Rating in its own small rectangular container */}
+      <div className="rating-container">
+        <p style={{ marginBottom: '5px' }}>Rating</p>
+        {/* CREATE COMPONENT FOR STARS or /100 FOR RATING */}
+        <div className="stars-container">
+          {/* Insert stars based on the game's rating */}
+        </div>
+      </div>
+      {/* List of details// can edit and reformat however */}
+      <div className="details-list">
+        <p>Genre: {game.genre}</p>
+        <p>System: {game.system}</p>
+        <p>Developer: {game.developer}</p>
+        <p>Release Date: {game['release-date']}</p>
+        <p>Maturity Level: {game['maturity-level']}</p>
+      </div>
+      {/* // ADD THE FAVORITE FUNCTIONALITY// to be edited in CSS */}
+      <div className="favorite-container">
+        <button className="favorite-button">
+          <span role="img" aria-label="heart">💖</span> Favorite
+        </button>
+      </div>
+      {/* for reviews //to be edited in CSS */}
+      <div className="reviews-container">
+        {/* Display reviews */}
+      </div>
+      {/* Link to navigate back to the Games page */}
+      <Link to="/games">Back</Link>
     </div>
   );
 }
