79ae23c17e3d44fe41bfa4f921739ca7e79e544b
Shukri Hussein <husseinshukri@outlook.com>
2024-05-03 11:14:40 -0400

    login validation

--- client/src/components/LoginSignup.jsx
+++ client/src/components/LoginSignup.jsx
@@ -1,40 +1,96 @@
-import React, { useState } from 'react'
-import './LoginSignup.css'
+import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import './LoginSignup.css';
 
-import user_icon from '@/assets/person.png'
-import password_icon from '@/assets/password.png'
-import email_icon from '@/assets/email.png'
+import user_icon from '@/assets/person.png';
+import password_icon from '@/assets/password.png';
+import email_icon from '@/assets/email.png';
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('Required'),
+});
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('Required'),
+});
 
 function LoginSignup() {
-  const [action, setAction] = useState("Sign Up");
+  const [action, setAction] = useState('Sign Up');
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 400);
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <div className="header">
         <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
-      <div className="inputs">
-        <div className='input'>
-          <img src={user_icon} alt="" />
-          <input type="text" placeholder='Name'/>
-        </div>
-        <div className='input'>
-          <img src={email_icon} alt="email" />
-          <input type="email" placeholder='Email'/>
-        </div>
-        <div className='input'>
-          <img src={password_icon} alt="password" />
-          <input type="password" placeholder='Password'/>
-        </div>
-      </div>
-      <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
-      <div className='submit-container'>
-        <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => {setAction("Sign Up")}}>Sign Up</div>
-        <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => {setAction("Login")}}>Login</div>
-      </div>
+      <Formik
+        initialValues={{ name: '', email: '', password: '' }}
+        validationSchema={action === 'Sign Up' ? SignupSchema : LoginSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="inputs">
+              {action === 'Sign Up' && (
+                <div className="input">
+                  <img src={user_icon} alt="" />
+                  <Field type="text" name="name" placeholder="Name" />
+                  <ErrorMessage name="name" component="div" className="error" />
+                </div>
+              )}
+              <div className="input">
+                <img src={email_icon} alt="email" />
+                <Field type="email" name="email" placeholder="Email" />
+                <ErrorMessage name="email" component="div" className="error" />
+              </div>
+              <div className="input">
+                <img src={password_icon} alt="password" />
+                <Field type="password" name="password" placeholder="Password" />
+                <ErrorMessage name="password" component="div" className="error" />
+              </div>
+            </div>
+            <div className="forgot-password">
+              Lost Password? <span>Click Here!</span>
+            </div>
+            <div className="submit-container">
+              <button
+                type="button"
+                className={action === 'Sign Up' ? 'submit' : 'submit gray'}
+                onClick={() => setAction('Sign Up')}
+              >
+                Sign Up
+              </button>
+              <button
+                type="button"
+                className={action === 'Login' ? 'submit' : 'submit gray'}
+                onClick={() => setAction('Login')}
+              >
+                Login
+              </button>
+              <button
+                type="submit"
+                className="submit-post"
+                disabled={isSubmitting}
+              >
+                Confirm
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
-  )
+  );
 }
 
-export default LoginSignup
+export default LoginSignup;
