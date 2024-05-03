ceacc1346bd1df6c5d8f743817f77809c8cdb8e6
Jasen <jasen.miyamoto@gmail.com>
2024-05-03 16:28:50 -0400

    test data from json now populates when seed.py is run

--- server/seed.py
+++ server/seed.py
@@ -1,6 +1,34 @@
 from app import app
 from models import db, User, Game, GameStatistics
 from faker import Faker
+import json
+from os.path import join, dirname
+
+def create_games():
+    # Construct the file path to JSON data
+    file_path = join(dirname(__file__), '../client/src/assets/TEST.json')
+
+    # Load the JSON data
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    for game_data in data:
+        game = Game(
+            game_name=game_data['name'],
+            genre=game_data['genre'],
+            developer=game_data['developer'],
+            description=game_data['description'],
+            release_date=str(game_data['release-date']),
+            maturity_level=game_data['maturity-level'],
+            system=game_data['system'],
+            rating=game_data['rating'],
+            image=game_data['image']
+        )
+        db.session.add(game)
+
+    # Commit the changes to the database
+    db.session.commit()
+
 
 fake = Faker()
 
@@ -14,33 +42,12 @@ def create_users(num_users):
     db.session.add_all(users)
     db.session.commit()
 
-def create_games(num_games):
-    games = []
-    ratings = ['Everyone', 'Teen', 'Mature']
-    for _ in range(num_games):
-        game_name = fake.catch_phrase()
-        genre = fake.word()
-        developer = fake.company()
-        maturity_level = fake.random_element(ratings)
-        system = fake.word()
-        description = fake.sentence()
-        rating = fake.random_number()
-        image = 'https://upload.wikimedia.org/wikipedia/en/c/c6/The_Legend_of_Zelda_Breath_of_the_Wild.jpg'
-        release_date = fake.date_between(start_date='-30y', end_date='today').strftime('%Y-%m-%d')
-        game = Game(game_name=game_name, genre=genre, developer=developer,
-                    description=description, release_date=release_date, maturity_level=maturity_level,
-                    system=system, rating=rating, image=image)
-        games.append(game)
-    db.session.add_all(games)
-    db.session.commit()
-
 with app.app_context():
     db.drop_all()
     db.create_all()
 
     create_users(3) # Create 3 users
-    create_games(10) # Create 10 games
-
+    create_games()
     # Create game statistics
     users = User.query.all()
     games = Game.query.all()
