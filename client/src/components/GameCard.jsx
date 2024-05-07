f0ea5610b9d2a44fb1b43790b83e4e98dfad4181
Shukri Hussein <husseinshukri@outlook.com>
2024-05-07 12:57:22 -0400

    material-ui

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -1,27 +1,33 @@
-import React, { useState } from 'react';
-import './GameCard.css';
+import React from 'react';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const GameCard = ({ game }) => {
-  const [expanded, setExpanded] = useState(false);
-
-  const toggleDescription = () => {
-    setExpanded(!expanded);
-  };
-
   return (
-    <div className="game-card">
-      <img src={game.image} alt={game.game_name} />
-      <Link to={`/games/${game.id}`}>
-        <h2>{game.game_name}</h2>
-      </Link>
-      <p>Genre: {game.genre}</p>
-      <p>Rating: {game.rating ? game.rating : 'N/A'} ⭐</p> 
-      {/* need to write logic to display stars for rating or rating out of 100? */}
-      <p>Description: {expanded ? game.description : game.description.slice(0, 35) + '...'}
-        <button onClick={toggleDescription}>{expanded ? 'Read less' : 'Read more'}</button>
-      </p>
-    </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="280"
+        image={game.image}
+        alt={game.game_name}
+      />
+      <CardContent>
+        <Link to={`/games/${game.id}`}>
+          <Typography gutterBottom variant="h5" component="div">
+            {game.game_name}
+          </Typography>
+        </Link>
+        <Typography variant="body2" color="text.secondary">
+          Genre: {game.genre}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Rating: {game.rating ? game.rating : 'N/A'} ⭐
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Description: {game.description}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
