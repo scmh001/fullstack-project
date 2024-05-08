b1e91aa063d70401c2bde8c4504101e8ec5b0e9a
Jasen <jasen.miyamoto@gmail.com>
2024-05-08 14:21:25 -0400

    PAIR PROGRAMMING Kristen and Michael - fixed comments issue

--- server/app.py
+++ server/app.py
@@ -146,7 +146,7 @@ api.add_resource(UsersById, '/users/<int:id>')
 class GameStatsByGameID(Resource):
     #this gets ALL comments, ratings for a specific game to be displayed on the individual game page 
     def get(self,game_id): 
-        gamestats = [gamestat.to_dict() for gamestat in GameStatistics.query.filter(GameStatistics.game_id==game_id).all()]
+        gamestats = [gamestat.to_dict() for gamestat in GameStatistics.query.filter(GameStatistics.game_id==game_id, GameStatistics.comments != None).all()]
         if gamestats:
             return make_response(gamestats)
         else:
