181a6a877e2a3b685a56b1aff2501a1b044e95d9
Jasen <jasen.miyamoto@gmail.com>
2024-05-08 13:31:00 -0400

    fixed gamedetail bug and review card render

--- server/app.py
+++ server/app.py
@@ -8,31 +8,31 @@ from sqlalchemy import desc
 from config import app, db, api
 
 
-
-@app.route('/users', methods=['POST'])
+# routes for login and user authentication 
+@app.route('/users', methods=['POST']) #sign up route
 def manage_users():
         data = request.json
-        new_user = User(username=data.get('username'))
+        new_user = User(username=data.get('username')) #create new user instance 
         new_user.password_hash = data.get('password')  # Set the password_hash using the setter
         db.session.add(new_user)
         db.session.commit()
-        session['user_id'] = new_user.id
+        session['user_id'] = new_user.id #set session hash to user id to keep logged in
         response = make_response(new_user.to_dict())
         response.set_cookie('user_id', str(new_user.id))
         return response, 201
 
 @app.route('/logout', methods=["GET"])
 def logout():
-    session['user_id'] = None 
+    session['user_id'] = None #clear session hash
     response = make_response({})
     response.delete_cookie('user_id')
     return response, 200
 
-@app.route('/authenticate-session')
+@app.route('/authenticate-session') #route for authentication 
 def authorize():
-    cookie_id = request.cookies.get('user_id')
+    cookie_id = request.cookies.get('user_id')  
     if cookie_id:
-        user = User.query.filter_by(id=cookie_id).first()
+        user = User.query.filter_by(id=cookie_id).first() #check to see if cookie matches current user id
         if user:
             return make_response(user.to_dict(only=['id', 'username'])), 200
     return make_response({'message': 'failed to authenticate'}), 401
@@ -40,15 +40,16 @@ def authorize():
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    user = User.query.filter_by(username=data.get('username')).first()
-    password = data.get('password')
-    if user and user.authenticate(password):
-        session['user_id'] = user.id
+    user = User.query.filter_by(username=data.get('username')).first() #check to see if username exists in db
+    password = data.get('password') 
+    if user and user.authenticate(password): #check entered password against encrypted password in db 
+        session['user_id'] = user.id 
         response = make_response(user.to_dict())
         response.set_cookie('user_id', str(user.id))
         return response, 200
     return jsonify({'message': 'Invalid username or password'}), 401
 
+# route to get all game stats for a user to display on their profile page 
 @app.route('/reviews/<int:user_id>', methods=['GET'])
 def get_reviews(user_id):
     reviews = [gamestat.to_dict() for gamestat in GameStatistics.query.filter(GameStatistics.user_id==user_id, GameStatistics.comments != None ).all()]
@@ -57,7 +58,7 @@ def get_reviews(user_id):
     else:
         return make_response({'error': ['No games reviewed yet']})
 
-
+# route to get 5 comments made to display on home page
 class RecentReviews(Resource):
     def get(self):
         games = [game.to_dict() for game in GameStatistics.query.filter(GameStatistics.comments != None).order_by(GameStatistics.rating.desc()).limit(5).all()]
@@ -66,6 +67,7 @@ class RecentReviews(Resource):
     
 api.add_resource(RecentReviews, '/recent_reviews')
 
+#route to get all games in db
 class Games(Resource):
     def get(self):
         games = [game.to_dict() for game in Game.query.all()]
@@ -74,6 +76,7 @@ class Games(Resource):
     
 api.add_resource(Games, '/games')
 
+#route to order games by rating for most popular page 
 class GamesByRating(Resource):
     def get(self):
         games = [game.to_dict() for game in Game.query.order_by(Game.rating.desc()).all()]
@@ -82,6 +85,7 @@ class GamesByRating(Resource):
     
 api.add_resource(GamesByRating, '/games-by-rating')
 
+#route that to get top 5 rated games to display on home page 
 class TopGames(Resource):
     def get(self):
         games = [game.to_dict() for game in Game.query.order_by(Game.rating.desc()).limit(5).all()]
@@ -90,6 +94,7 @@ class TopGames(Resource):
     
 api.add_resource(TopGames, '/top-games')
 
+#route to get details for specific individual game page 
 class GamesById(Resource):
     def get(self, id):
         game = Game.query.filter(Game.id==id).first()
@@ -100,6 +105,7 @@ class GamesById(Resource):
     
 api.add_resource(GamesById, '/games/<int:id>')
 
+#route to access user info and delete user 
 class UsersById(Resource):
     def get(self, id):
         user = User.query.filter(User.id==id).first()
@@ -136,9 +142,9 @@ class UsersById(Resource):
 
 api.add_resource(UsersById, '/users/<int:id>')
 
-#jasen might be wrong about how this should work and is willing to accept responsibility 
+#jasen was NOT wrong about how this works! 
 class GameStatsByGameID(Resource):
-    #this gets ALL comments, ratings for a specific game NOT BY USER
+    #this gets ALL comments, ratings for a specific game to be displayed on the individual game page 
     def get(self,game_id): 
         gamestats = [gamestat.to_dict() for gamestat in GameStatistics.query.filter(GameStatistics.game_id==game_id).all()]
         if gamestats:
@@ -149,7 +155,7 @@ class GameStatsByGameID(Resource):
 api.add_resource(GameStatsByGameID, '/game-statistic/<int:game_id>')
 
 class GameStatsByUserAndGameIDs(Resource):
-    #this allows a user to patch, passing both ids in (i think)
+    #this allows a user to patch (favorite, wishlist, comment, rate)
     def get(self, game_id, user_id):
         gamestat = GameStatistics.query.filter(GameStatistics.game_id == game_id, GameStatistics.user_id == user_id).first()
         if gamestat:
@@ -185,8 +191,8 @@ class GameStatsByUserAndGameIDs(Resource):
 
 api.add_resource(GameStatsByUserAndGameIDs, '/game-statistics/<int:game_id>/<int:user_id>')
 
+#route for creating a new GameStatistic instance when a user comments, rates, favorites or wishlists for the first time
 class GameStats(Resource):
-    #is this right???
     def post(self):
         data = request.json
         try:
@@ -209,6 +215,8 @@ class GameStats(Resource):
             return make_response({'error': ['validation errors']}, 400)
     
 api.add_resource(GameStats, '/game-statistics')
+
+# route for accessing a user's favorites to display on favorites page
 class FavoritesByUser(Resource):
     def get(self, user_id):
         favorites = [gamestat.game.to_dict() for gamestat in GameStatistics.query.filter(GameStatistics.user_id==user_id, GameStatistics.favorited==True).all()]
@@ -219,6 +227,7 @@ class FavoritesByUser(Resource):
         
 api.add_resource(FavoritesByUser, '/favorites/<int:user_id>')
 
+# route for accessing a user's wishlisted items to display on wishlist page
 class WishlistByUser(Resource):
     def get(self, user_id):
         wishlisted = [gamestat.game.to_dict() for gamestat in GameStatistics.query.filter(GameStatistics.user_id==user_id, GameStatistics.wish_listed==True).all()]
@@ -229,6 +238,7 @@ class WishlistByUser(Resource):
         
 api.add_resource(WishlistByUser, '/wishlist/<int:user_id>')
 
+#route for search queries for searchbar 
 @app.route('/search', methods=['GET'])
 def search_games():
     query = request.args.get('q')
